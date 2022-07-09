0d1b0a368c142c639f9d6e66321370afec192c1c
samaco634 <72288990+samaco634@users.noreply.github.com>
2022-07-09 11:27:05 +0900

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,70 +1,77 @@
 import RPi.GPIO as GPIO
 import time
 
-PWMA = 18
-AIN1 = 24
-AIN2 = 25
+class CarMotor():
 
-PWMB = 23
-BIN1 = 22
-BIN2 = 27
+    def __init__(self):
+       self.PWMA = 18
+       self.AIN1 = 24 #27
+       self.AIN2 = 25 #22
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(PWMA,GPIO.OUT)
-GPIO.setup(AIN1,GPIO.OUT)
-GPIO.setup(AIN2,GPIO.OUT)
-
-GPIO.setup(PWMB,GPIO.OUT)
-GPIO.setup(BIN1,GPIO.OUT)
-GPIO.setup(BIN2,GPIO.OUT)
+       self.PWMB = 23
+       self.BIN1 = 22 #25
+       self.BIN2 = 27 #24
 
 
-L_Motor = GPIO.PWM(PWMA,500)
-L_Motor.start(0)
+       GPIO.setwarnings(False)
+       GPIO.setmode(GPIO.BCM)
+       GPIO.setup(self.PWMA, GPIO.OUT)
+       GPIO.setup(self.AIN1, GPIO.OUT)
+       GPIO.setup(self.AIN2, GPIO.OUT)
 
-R_Motor = GPIO.PWM(PWMB,500)
-R_Motor.start(0)
+       GPIO.setup(self.PWMB, GPIO.OUT)
+       GPIO.setup(self.BIN1, GPIO.OUT)
+       GPIO.setup(self.BIN2, GPIO.OUT)
 
-def motor_forward(speed):
-    GPIO.output(AIN1,0)
-    GPIO.output(AIN2,1)
-    L_Motor.ChangeDutyCycle(speed)
+       self.L_Motor = GPIO.PWM(self.PWMA, 500)
+       self.L_Motor.start(0)
+
+       self.R_Motor = GPIO.PWM(self.PWMB, 500)
+       self.R_Motor.start(0)
 
 
-def motor_backward(speed):
-    pass
+    def motor_forward(self, speed):
+        if speed > 100:
+            speed = 100
+        GPIO.output(self.AIN1, 0)
+        GPIO.output(self.AIN2, 1)
+        
+        self.L_Motor.ChangeDutyCycle(speed)
+ 
+    def motor_stop(self):   
+        self.L_Motor.ChangeDutyCycle(0)
+        self.R_Motor.ChangeDutyCycle(0)
+        
+    def motor_backward(self, speed):
+        if speed > 100:
+            speed = 100
 
+        
+    def  motor_left(self, speed):
+        if speed > 100:
+            speed = 100
     
-def motor_left(speed):
-    pass
-    
-def motor_right(speed):
-    pass
-
-
-def motor_stop():
-    pass
+    def  motor_right(self, speed):
+        if speed > 100:
+            speed = 100
 
 if __name__ == '__main__':
 
+    car = CarMotor()
     try:
         while True:
-            motor_forward(100)
+            car.motor_forward(100)
             time.sleep(2)
-            motor_stop()
+            car.motor_stop()
             time.sleep(2)
-            motor_left(100)
+            car.motor_left(100)
             time.sleep(2)
-            motor_stop()
+            car.motor_stop()
+            time.sleep(2)        
+            car.motor_backward(100)
             time.sleep(2)
- 
-            motor_backward(100)
-            time.sleep(2)
-            motor_stop()
+            car.motor_stop()
             time.sleep(2)
     except KeyboardInterrupt:
         pass
-
     GPIO.cleanup()
