ceef58ec75bb43579c0fe48d7ce38d3f6e5d2651
samaco634 <72288990+samaco634@users.noreply.github.com>
2022-08-06 04:43:33 +0900

    Create data_compress.py

---
+++ data_compress.py
@@ -0,0 +1,20 @@
+import sys
+import os 
+import glob
+import zipfile
+
+def main():
+
+    condition =  "./data/*/*.png"
+
+    png_files = glob.glob(condition)
+    print(png_files)
+
+    with zipfile.ZipFile("car_image.zip", 'w') as my_zip:
+    #my_zip = zipfile.ZipFile("car_image_angle.zip", 'w')
+        for file in png_files:
+            my_zip.write(file)
+    my_zip.close()
+
+if __name__ =='__main__':
+	main()
